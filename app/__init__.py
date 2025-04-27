4f0525310c4d9a07869aa339b08a48face127a13
murata <finesea_bluesky@icloud.com>
2025-04-27 18:46:16 +0900

    テスト運用開始

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../static")
     app.config.from_object(config_class)
 
     db.init_app(app)
